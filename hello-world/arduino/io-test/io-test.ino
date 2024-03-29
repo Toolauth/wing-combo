eefa887b634f77d9e54822345f554cc41eca8346
Corey Rice <crice009@gmail.com>
2024-03-29 00:50:00 -0400

    start Arduino hello-world

---
+++ hello-world/arduino/io-test/io-test.ino
@@ -0,0 +1,23 @@
+// pin identifications
+#define pin_sda 21
+#define pin_scl 22
+#define pin_irq 25
+#define pin_red_led 32
+#define pin_green_led 33
+#define pin_reset_relay 17
+#define pin_set_relay 16
+#define pin_buzzer 5
+#define pin_bypass_key 26
+#define pin_estop_1 27
+#define pin_estop_2 14
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
